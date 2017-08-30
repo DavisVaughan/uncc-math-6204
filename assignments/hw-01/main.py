5e147b0a99cba345b89a52a47a7b5dda781637ee
DavisVaughan <mvaugh15@uncc.edu>
2017-08-30 10:39:19 -0400

    Add put and documentation

---
+++ assignments/hw-01/main.py
@@ -0,0 +1,17 @@
+from call_option_value import V_eur_call, V_eur_put
+
+def main():
+
+    # Parameters
+    k          = 100
+    s_0        = 100
+    t          = 0
+    t_terminal = 1
+    div_yield  = 0.025
+    r          = 0.05
+    sigma      = 0.10
+
+    print(V_eur_call(s_0, k, r, div_yield, t_terminal, t, sigma))
+    print( V_eur_put(s_0, k, r, div_yield, t_terminal, t, sigma))
+
+main()
