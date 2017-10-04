f83b90c966b0eca78d1ca4071eab357a7d44fce2
DavisVaughan <mvaugh15@uncc.edu>
2017-10-04 09:47:49 -0400

    Update documentation

--- assignments/hw-04/option_value.py
+++ assignments/hw-04/option_value.py
@@ -3,16 +3,34 @@ import sys
 import scipy.optimize as scipy_opt
 
 def price_option(s, k, r, T, call_put = "call", option_type = "european"):
-    '''
-    Calculate the time 0 value of an option from Monte Carlo simulations
-    :param s:           An array of the simulated paths of the option
-    :param k:           The strike price for the option
-    :param r:           The risk free interest rate to discount at
-    :param T:           The number of years to discount
-    :param call_put:    Either "call" or "put"
-    :param option_type: The type of option. "european" or "american"
-    :return:            The Monte Carlo value of the option
-    '''
+    """
+    Calculate the time 0 value of an option from Monte Carlo simulations.
+
+    Parameters
+    ----------
+    s : Numpy array
+        The simulated paths of the option.
+    k : double
+        The strike price for the option.
+    r : double
+        The risk free interest rate to discount at.
+    T : double
+        The expiration date of the option. Used to discount.
+    call_put : {"call", "put"}, default "call"
+        Specifications for the side of the option to price.
+    option_type : {"european", "american"}, default "european"
+        The type of option to price.
+
+    Returns
+    ----------
+    price : double
+        The Monte Carlo value of the option
+
+    See also
+    ----------
+    simulate_gbm
+
+    """
 
     # Error checking
     if call_put not in ["call", "put"]:
@@ -40,25 +58,7 @@ def dispatch_pricing_method(call_put = "call", option_type = "european"):
     :return:             A function that prices the specified type of option
     '''
 
-    if option_type == "european":
-
-        pricing_method = european(call_put)
-
-    elif option_type == "american":
-
-        pricing_method = american(call_put)
-
-    return pricing_method
-
-
-def european(call_put = "call"):
-    '''
-    Return a function that prices a European call/put
-    :param call_put:  Either a "call" or "put"
-    :return:          European pricing function
-    '''
-
-    # Get the function specific to a european call or put
+    # Get the function specific to a call or put option price at time T
     if call_put == "call":
 
         option_price_at_T = option_price_at_T_call
@@ -67,6 +67,25 @@ def european(call_put = "call"):
 
         option_price_at_T = option_price_at_T_put
 
+    # Get the pricing method specific to european or american
+    if option_type == "european":
+
+        pricing_method = european(option_price_at_T)
+
+    elif option_type == "american":
+
+        pricing_method = american(option_price_at_T)
+
+    return pricing_method
+
+
+def european(call_put_option_price_at_T):
+    '''
+    Return a function that prices a European call/put
+    :param call_put_option_price_at_T:  A pricing function that prices the value of the option at T. This is different
+    depending on whether the option is a call or a put.
+    :return:          European pricing function
+    '''
 
     # Create the European pricing function
     def european_pricing_method(s, k, r, T):
@@ -84,7 +103,7 @@ def european(call_put = "call"):
         s_T = s[:, -1]
 
         # Apply call / put option value at T
-        v_T = option_price_at_T(s_T, k)
+        v_T = call_put_option_price_at_T(s_T, k)
 
         # Discount
         v_0 = np.exp(- r * T) * np.mean(v_T)
@@ -94,22 +113,13 @@ def european(call_put = "call"):
     return european_pricing_method
 
 
-def american(call_put = "call"):
+def american(call_put_option_price_at_T):
     '''
     Return a function that prices an American call/put
-    :param call_put:  Either a "call" or "put"
+    :param call_put_option_price_at_T:  A pricing function that prices the value of the option at T. This is different
+    depending on whether the option is a call or a put. The function should accept s_T and k as parameters.
     :return:          American pricing function
     '''
-
-    # Get the function specific to a european call or put
-    if call_put == "call":
-
-        option_price_at_T = option_price_at_T_call
-
-    elif call_put == "put":
-
-        option_price_at_T = option_price_at_T_put
-
 
     def american_pricing_method(s, k, r, T):
         '''
@@ -126,7 +136,7 @@ def american(call_put = "call"):
         v = np.zeros(s.shape)
 
         # At i = M = T, use value at T
-        v[:, -1] = option_price_at_T(s[:, -1], k)
+        v[:, -1] = call_put_option_price_at_T(s[:, -1], k)
 
         # Basis function
         def basis_func_C(x, a0, a1, a2, a3):
@@ -149,7 +159,7 @@ def american(call_put = "call"):
             C_hat = basis_func_C(s_ik, *params)
 
             # Calculate the value of the option if this was time T
-            s_ik_v = option_price_at_T(s_ik, k)
+            s_ik_v = call_put_option_price_at_T(s_ik, k)
 
             # Take the max of the two to be the current value
             v[:, i] = np.maximum(s_ik_v, C_hat)
